d01c7040a8e641219bee59500be8ef3cb63d5c68
danielxu04 <danielxu004@gmail.com>
2022-07-23 18:26:15 -0400

    project setup, created and modified JFrame object (framed window for game)

---
+++ Main.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class Main {
+    
+    public static void main(String args[]){
+
+        // create a JFrame object
+        JFrame gameWindow = new JFrame();
+        gameWindow.setTitle("Code Man!"); // set name of game
+        gameWindow.setResizable(false);
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
